7422ea3dd00ecfdc9f6441676725a444138886c3
Kylie Benfield <kylie@revelryplay.com>
2024-03-10 17:24:29 -0600

    Adding initial library files

---
+++ ArgoDraft.cpp
@@ -0,0 +1,15 @@
+#include "ArgoDraft.h"
+
+#include <iostream>
+
+namespace ArgoDraft
+{
+    void about()
+    {
+        std::cout << "This library is for work in progress concepts for the Argo Game Engine" << std::endl;
+        std::cout << "It is not intended for production use." << std::endl;
+        std::cout << "The implementations in this library are incomplete and poorly implemented." << std::endl;
+        std::cout << "Use at your own risk." << std::endl;
+        std::cout << "For more information, visit https://docs.revelryplay.com/argo/first-draft/" << std::endl;
+    }
+}
